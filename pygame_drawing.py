7c639d28bc34372aee69021c6e2d6c2f59ea1919
miaa-zhou <miazhou94@gmail.com>
2026-01-12 14:36:42 -0800

    Add files via upload

---
+++ pygame_drawing.py
@@ -0,0 +1,59 @@
+# Pygame Drawing
+# Author: Mia Zhou
+# Date: 2023-09-20
+
+import pygame
+
+def game():
+    pygame.init()
+
+    # COLOURS - (R, G, B)
+    # CONSTANTS ALL HAVE CAPS FOR THEIR NAMES
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    GREY = (128, 128, 128)
+
+    # CONSTANTS
+    WIDTH = 800
+    HEIGHT = 600
+    SIZE = (WIDTH, HEIGHT)
+
+    # Creating the Screen
+    screen = pygame.display.set_mode(SIZE)
+    pygame.display.set_caption("Your Title Here")
+
+    # Variables
+    done = False
+    clock = pygame.time.Clock()
+
+    # ------------ MAIN GAME LOOP
+    while not done:
+        # ------ MAIN EVENT LISTENER
+        # when the user does something
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        # ------ GAME LOGIC
+
+        # ------ DRAWING TO SCREEN
+        screen.fill(WHITE)
+        pygame.draw.circle(screen, RED, (200, 200), 120)
+        pygame.draw.circle(screen, BLACK, (160, 170), 15)
+        pygame.draw.circle(screen, BLACK, (240, 170), 15)
+        pygame.draw.arc(screen, BLACK, (140, 180, 120, 80), 3.14, 6.28, 4)
+
+        # Update screen
+        pygame.display.flip()
+
+        # ------ CLOCK TICK
+        clock.tick(60)  # 60 fps
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    game()
